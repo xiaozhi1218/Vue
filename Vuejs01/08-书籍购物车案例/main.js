490749ef29b049f46d53df9a6f53efe11e1b7c69
chenyongzhi <chenyongzhijjk@163.com>
2019-10-30 22:03:03 +0800

    demo

--- Vuejs01/08-书籍购物车案例/main.js
+++ Vuejs01/08-书籍购物车案例/main.js
@@ -36,6 +36,45 @@ const app = new Vue({
     // getFinalPrice(price) {
     //   return '￥' + price.toFixed(2)
     // }
+    increment(index) {
+      this.books[index].count++
+    },
+    decrement(index) {
+      this.books[index].count--
+    },
+    removeHandle(index) {
+      this.books.splice(index, 1)
+    }
+  },
+  computed: {
+    totalPrice() {
+      // 1.普通的for循环
+      // let totalPrice = 0
+      // for (let i = 0; i < this.books.length; i++) {
+      //   totalPrice += this.books[i].price * this.books[i].count
+      // }
+      // return totalPrice
+
+      // 2. for(let i in this.books)
+      // let totalPrice = 0
+      // for (let i in this.books) {
+      //   const book = this.books[i]
+      //   totalPrice += book.price * book.count
+      // }
+      // return totalPrice
+
+      // 3. for (let i of this.books)
+      // let totalPrice = 0
+      // for (let item of this.books) {
+      //   totalPrice += item.price * item.count
+      // }
+      // return totalPrice
+
+      // reduce
+      return this.books.reduce(function(preValue, book) {
+        return preValue + book.price * book.count
+      }, 0)
+    }
   },
   filters: {
     showPrice(price) {
