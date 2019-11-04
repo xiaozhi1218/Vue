b71871a0384c5a57d27954370da6e7a43571c1bd
chenyongzhi <chenyongzhijjk@163.com>
2019-11-04 09:14:41 +0800

    webpack loader

--- Vuejs02/01-webpack的使用/03-webpack的loader/webpack.config.js
+++ Vuejs02/01-webpack的使用/03-webpack的loader/webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // css-loader只负责将css文件进行加载
+        // style-loader负责将样式添加到DOM中
+        // 使用多个loader时, 是从右向左
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
   }
 }
