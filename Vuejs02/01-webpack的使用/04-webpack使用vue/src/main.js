f4867627a0efb5e3415f591f53e5a36a855531ac
chenyongzhi <chenyongzhijjk@163.com>
2019-11-05 22:40:26 +0800

    vue

---
+++ Vuejs02/01-webpack的使用/04-webpack使用vue/src/main.js
@@ -0,0 +1,42 @@
+// 1.使用commonjs的模块化规范
+const {add, mul} = require('./js/mathUtils.js')
+
+console.log(add(20, 30));
+console.log(mul(20, 30));
+
+// 2.使用ES6的模块化的规范
+import {name, age, height} from "./js/info";
+
+console.log(name);
+console.log(age);
+console.log(height);
+
+// 3.依赖css文件
+require('./css/normal.css')
+
+// 4.依赖less文件
+require('./css/special.less')
+document.writeln('<h2>hello,world</h2>')
+
+// 5. 使用Vue进行开发
+import Vue from 'vue'
+
+new Vue({
+  el: '#app',
+  template: `
+    <div>
+      <h2>{{message}}</h2>
+      <button @click="btnClick">按钮</button>
+      <h2>{{name}}</h2>
+    </div>
+  `,
+  data: {
+    message: 'hello Webpck',
+    name: 'cyz'
+  },
+  methods: {
+    btnClick() {
+
+    }
+  },
+})
