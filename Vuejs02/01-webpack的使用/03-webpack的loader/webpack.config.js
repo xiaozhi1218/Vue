6d6e60a4e4e110ee1bf282ec33b5c91478583f5c
chenyongzhi <chenyongzhijjk@163.com>
2019-11-05 13:27:09 +0800

    babel-loader

--- Vuejs02/01-webpack的使用/03-webpack的loader/webpack.config.js
+++ Vuejs02/01-webpack的使用/03-webpack的loader/webpack.config.js
@@ -8,13 +8,12 @@ module.exports = {
     publicPath: 'dist/'
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.css$/,
         // css-loader只负责将css文件进行加载
         // style-loader负责将样式添加到DOM中
         // 使用多个loader时, 是从右向左
-        use: [ 'style-loader', 'css-loader' ]
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.less$/,
@@ -28,20 +27,20 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|gif|jpeg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              // 当加载的图片, 小于limit时, 会将图片编译成base64字符串形式.
-              // 当加载的图片, 大于limit时, 需要使用file-loader模块进行加载.
-              limit: 13000,
-              name: 'img/[name].[hash:8].[ext]'
-            },
-          }
-        ]
+        use: [{
+          loader: 'url-loader',
+          options: {
+            // 当加载的图片, 小于limit时, 会将图片编译成base64字符串形式.
+            // 当加载的图片, 大于limit时, 需要使用file-loader模块进行加载.
+            limit: 13000,
+            name: 'img/[name].[hash:8].[ext]'
+          },
+        }]
       },
       {
         test: /\.js$/,
+        // exclude: 排除
+        // include: 包含
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
