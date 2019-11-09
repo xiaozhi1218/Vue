949914747abedda4738b6479f46864f860047895
chenyongzhi <chenyongzhijjk@163.com>
2019-11-09 13:43:36 +0800

    tabbar component

---
+++ Vuejs03/06-tabbar/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <tab-bar>
+      <tab-bar-item></tab-bar-item>
+      <tab-bar-item></tab-bar-item>
+      <tab-bar-item></tab-bar-item>
+      <tab-bar-item></tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from "./components/tabbar/TabBar"
+import TabBarItem from './components/tabbar/TabBarItem'
+
+export default {
+  name: "App",
+  components: {
+    TabBar,
+    TabBarItem
+  }
+};
+</script>
+
+<style>
+@import "./assets/css/base.css";
+</style>
